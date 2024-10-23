5d44066521ef1751f17a7ceda76ba552db55d076
cyj <cyj1116@outlook.com>
2024-10-24 02:42:28 +0800

    Updates

--- assets/index-C45xU-9a.js
+++
@@ -1,1 +0,0 @@
-import{r,a as t}from"./index-iEVe0ZmB.js";const a=r.memo(()=>t("div",{children:"Entire"}));export{a as default};
--- assets/index-DInC6kQN.js
+++
@@ -1,1 +0,0 @@
-import{r,a as t}from"./index-iEVe0ZmB.js";const e=r.memo(()=>t("div",{children:"Detail"}));export{e as default};
