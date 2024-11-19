79460ae38ec1d92b7b0e6fd05ff1748c992e19ab
cyj <cyj1116@outlook.com>
2024-11-19 10:30:33 +0800

    feat: change to next

--- src/components/app-footer/index.tsx
+++
@@ -1,7 +0,0 @@
-import React, { memo } from "react"
-
-const AppFooter = memo(() => {
-  return <div>Footer</div>
-})
-
-export default AppFooter
--- src/views/home/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-const Home: React.FC = () => {
-  return (
-    <div>{/* <Button onClick={handleNavigate}>Go to Step</Button>{" "} */}</div>
-  )
-}
-
-export default Home
